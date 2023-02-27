c8099438a612d6d3974e18ec8bf1b1ddf65331e9
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-02-28 01:49:07 +0900

    feat: size 테스트 구현
    학습테스트 실습 - Set Collection
    
    요구사항 1 size() 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,10 @@ package study;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -15,5 +19,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 길이 확인 테스트")
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
