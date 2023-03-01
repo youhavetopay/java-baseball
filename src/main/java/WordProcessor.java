4502373a3f5a72c6acfddbb4017faab2272204c2
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-03-01 23:06:57 +0900

    feat: 문자열에서 숫자 추출 기능 구현
    - 문자열을 공백기준으로 split
    - 숫자만 체크해서 List에 담아서 리턴

--- src/main/java/WordProcessor.java
+++ src/main/java/WordProcessor.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WordProcessor {
@@ -6,5 +8,31 @@ public class WordProcessor {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
-    
+
+    List<Integer> splitStringByNumber(String input){
+        String[] splitWords = input.split(" ");
+        List<Integer> numbers = new ArrayList<>();
+
+        for(String word : splitWords){
+            addIntegerWord(numbers, word);
+        }
+
+        return numbers;
+    }
+
+    void addIntegerWord(List<Integer> numbers, String word){
+        if (checkNumberFormat(word)) {
+            numbers.add(Integer.parseInt(word));
+        }
+    }
+
+    boolean checkNumberFormat(String word){
+        try{
+            Integer.parseInt(word);
+        } catch (NumberFormatException numberFormatException){
+            return false;
+        }
+        return true;
+    }
+
 }
